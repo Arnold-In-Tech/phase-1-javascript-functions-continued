ee299b18a20ab8a205480063948fc2fde67c8f5f
Arnold-In-Tech <g16a7782@gmail.com>
2023-12-14 04:07:20 +0300

    Completed Assnigment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // code your solution here
+
+// let game = 'roller-skate!'
+function saturdayFun(game, deflt = 'roller-skate'){
+    if (game != null){
+        return `This Saturday, I want to ${game}!`;
+    }
+    else{
+        return `This Saturday, I want to ${deflt}!`;
+    }
+}
+
+
+const mondayWork = function(act, dft = 'go to the office'){
+    if (act != null){
+        return `This Monday, I will ${act}.`;
+    }
+    else{
+        return `This Monday, I will ${dft}.`;
+    }
+}
+
+
+const wrapAdjective = (function(vsFlair = "*"){
+    return function(param1 = "special"){
+        return `You are ${vsFlair}${param1}${vsFlair}!`;
+    };
+})  
+
+wrapAdjective("%")("a dedicated programmer"); 
